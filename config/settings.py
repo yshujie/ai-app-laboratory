a4bae80b1849efb88429f5244432ee6190d60edf
yshujie <yshujie@163.com>
2024-06-24 11:33:31 +0800

    Ci use pydantic manage .enb

--- config/settings.py
+++ config/settings.py
@@ -1,9 +1,12 @@
 import os
+from pydantic import BaseSettings
 from dotenv import load_dotenv
 
 # 加载 .env 文件中的环境变量
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
 
-# 全局变量配置
+class Settings(BaseSettings):
+    class Config:
+        env_file = ".env"
 
-# 其他配置项...
+settings = Settings()
