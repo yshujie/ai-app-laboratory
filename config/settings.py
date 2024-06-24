cc629fff6a76f8a65bba560996297a7780bd0d1a
yshujie <yshujie@163.com>
2024-06-24 12:15:16 +0800

    Fix up docker err

--- config/settings.py
+++ config/settings.py
@@ -6,5 +6,13 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
 
 class Settings(BaseSettings):
+    debug: bool
+    port: int
+    http_proxy: str = None
+    https_proxy: str = None    
+    
     class Config:
         env_file = ".env"
+        extra = "allow"  # 允许额外的未定义字段
+
+settings = Settings()
--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from src.api.main import app
 from config.settings import settings
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=settings.PORT)
+    uvicorn.run(app, host="0.0.0.0", port=settings.port)
